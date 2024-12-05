69ff3e54d7d91bfc9ce92e6e5275f758508afb31
Vennamm <33382705+Vennamm@users.noreply.github.com>
2024-12-05 04:56:42 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -483,7 +483,7 @@ def temperature_forecasting():
             legend_title='Components',
             template='plotly_dark'  # Optional: Adjust the theme
         )
-        fig.write_image("trend_seasonal_residual.png")
+        # fig.write_image("trend_seasonal_residual.png")
         
         st.pyplot(fig)
     
