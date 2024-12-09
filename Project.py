a366bb0dfb27f28e2e80e8b8e8394d01c4613e1b
Vennamm <33382705+Vennamm@users.noreply.github.com>
2024-12-08 21:46:33 -0500

    Update Project.py

--- Project.py
+++ Project.py
@@ -688,7 +688,7 @@ st.markdown(
     </style>
     """,
     unsafe_allow_html=True
-)
+) 
 # section = st.sidebar.radio("Select a section", ["Documentation", "Temperature Forecasting", "Weather-Energy Relationship"])
 t1, t2, t3, t4 = st.tabs(["Documentation", "Temperature Forecasting", "Weather-Energy Relationship", "Future Goals"])
 with t1:
